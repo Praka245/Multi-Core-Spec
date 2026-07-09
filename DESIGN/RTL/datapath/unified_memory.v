0ce244b02fe51072457cd303bfb4310b69dabee6
Mohanapriyan p <mohanapriyanp.ec24@bitsathy.ac.in>
2026-07-09 15:25:11 +0530

    Add module documentation for unified_memory
    Added module header comments for unified_memory.v

--- DESIGN/RTL/datapath/unified_memory.v
+++ DESIGN/RTL/datapath/unified_memory.v
@@ -1,3 +1,28 @@
+/*
+==============================================================================
+ Project      : RV32I Multi-Cycle RISC-V Processor
+ Module No    : M2
+ Module Name  : unified_memory
+ File Name    : unified_memory.v
+
+ Core         : RV32I
+ Architecture : Multi-Cycle
+ ISA          : RV32I Base Integer ISA
+ XLEN         : 32-bit
+
+ Description  :  single memory module used for storing both instructions and data.
+
+
+ Date Created : 07.07.2026
+ Version      :
+
+ Inputs       : clk,reset,MemRead,MemWrite,write_data,address
+
+ Outputs      : read_data
+
+==============================================================================
+*/
+
 module unified_memory #(
 	parameter DATA_WIDTH = 32,
 	parameter ADDRESS_WIDTH = 32,
